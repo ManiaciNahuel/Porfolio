ed5bc0faf148285ecafb3c1b5a4b970036c27f69
Nahuel Maniaci <nahuelmaniaci@gmail.com>
2024-07-13 19:44:53 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Footer } from "./components/navigation/Footer";
 import { NavBar } from "./components/navigation/NavBar";
 import { AboutMe } from "./components/sections/AboutMe";
 import { Contact } from "./components/sections/Contact";
-import { Home } from "./components/sections/Home";/* 
-import { NewTry } from "./components/sections/NewTry"; */
+import { Home } from "./components/sections/Home";
 import { Projects } from "./components/sections/Projects";
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
       <div id="section">
-        <Home/>
-        <AboutMe/>
-        <Projects/>
-        <Contact/>
-        <Footer/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
-    </>
+      <Footer />
+    </Router>
   );
 }
 
